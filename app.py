fe053bdd4640e39945d0e70fcd3aa6332ec5021c
Stephen Warton <stephen.warton@gmail.com>
2017-08-01 15:13:30 -0600

    added get one routes

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # dependencies
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, json, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 # app config
@@ -35,6 +35,18 @@ def reviews():
             'user_id': review.user_id
         })
     return jsonify(all_data)
+
+
+@app.route('/api/v1/users/<user_id>')
+def getUserById(user_id):
+    user = models.Users.query.filter_by(id=user_id).first_or_404()
+    return jsonify({'id': user.id, 'email': user.email, 'password': user.password})
+
+
+@app.route('/api/v1/reviews/<review_id>')
+def getReviewById(review_id):
+    review = models.Reviews.query.filter_by(id=review_id).first_or_404()
+    return jsonify({'id': review.id, 'title': review.title, 'description': review.description, 'user_id': review.user_id})
 
 
 if __name__ == '__main__':
